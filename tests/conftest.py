d012a8d8cf7eeb6181aac2506755bdc4e7b81202
bindereif <florian.bindereif@mail.schwarz>
2025-06-11 13:43:58 +0200

    feat: create conftest fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,10 @@
+import pytest
+from fastapi.testclient import TestClient
+from app.api.main import app
+
+@pytest.fixture(scope="module")
+def test_client() -> TestClient:
+    """
+    Provides a FastAPI TestClient for making requests to the application during tests.
+    """
+    return TestClient(app)
