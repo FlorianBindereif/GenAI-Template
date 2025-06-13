7e3eee82f45b9ee7502e0476aeeb583ad77d5bf5
bindereif <florian.bindereif@mail.schwarz>
2025-06-13 16:22:17 +0200

    fix: extend unit tests with small docu

--- tests/unit/test_settings.py
+++ tests/unit/test_settings.py
@@ -17,7 +17,7 @@ def test_settings_override_with_env_vars(monkeypatch: pytest.MonkeyPatch) -> Non
     # Act: initialize Instance of setting object
     settings = Settings()
 
-    # Act: verify that
+    # Assert: verify expected behavior
     assert settings.APP_NAME == "My Awesome Test App"
     assert settings.LOG_LEVEL == "DEBUG"
 
@@ -27,14 +27,16 @@ def test_settings_loading_from_env_file(tmp_path: Path, monkeypatch: pytest.Monk
     Tests that settings are correctly loaded from a .env file.
     """
 
-    # Arrange: Initialize the service with the mocked environment
+    # Arrange: Set custom environment dotfile
     env_content = """OPENAI_API_KEY="test_api_key_from_dotenv"""
     env_file = tmp_path / ".env"
     env_file.write_text(env_content)
-
     monkeypatch.setitem(Settings.model_config, "env_file", env_file)
+
+    # Act: initialize Instance of setting object
     settings = Settings()
 
+    # Assert: verify expected behavior
     assert settings.OPENAI_API_KEY == "test_api_key_from_dotenv"
     assert settings.LOG_LEVEL == "INFO"
 
@@ -43,8 +45,11 @@ def test_settings_invalid_base_url(monkeypatch: pytest.MonkeyPatch) -> None:
     """
     Tests that settings can be overridden with environment variables.
     """
+
+    # Arrange: Set custom environment dotfile
     invalid_url = "https//api.openai.com/v1"
     monkeypatch.setenv("OPENAI_BASE_URL", invalid_url)
 
+    # Act / Assert: verify expected behavior
     with pytest.raises(ValidationError):
         Settings()
