f89f0a8e3e83691caafdea9c60a6696085cfefad
bindereif <florian.bindereif@mail.schwarz>
2025-06-15 22:58:49 +0200

    fix: correct linting issues

--- app/api/main.py
+++ app/api/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
+
 from app.api.endpoints import greetings
 from app.config import settings
 
 app = FastAPI(
     title=settings.APP_NAME,
-    description=settings.APP_DESCRIPTION
+    description=settings.APP_DESCRIPTION,
 )
 app.include_router(greetings.router, prefix=settings.API_PREFIX)
+
 
 @app.get("/", tags=["Root"])
 def read_root():
